e68645291a7246d1d742068fee0fdf18f31a3bc9
NotPhaden <ytnotphade@gmail.com>
2026-06-05 02:13:45 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+console.log("HYPER LYTE Loaded");
+
+document.querySelectorAll("a[href^='#']").forEach(anchor => {
+
+anchor.addEventListener("click", function(e){
+
+e.preventDefault();
+
+document.querySelector(this.getAttribute("href"))
+.scrollIntoView({
+behavior:"smooth"
+});
+
+});
+
+});
